54c2801ed93bf4f6018654f8f442c136f1c41314
tanshiyitsy <1365519162@qq.com>
2022-04-19 21:01:50 +0800

    Update bob.cpp

--- bob.cpp
+++ bob.cpp
@@ -4,43 +4,10 @@ using namespace std;
 
 static int send_fifo = 0;
 static int recv_fifo = 0;
-// void send(const Message *message)
-// {
-//     //static int send_fifo = 0;
-//     if (send_fifo == 0)
-//     {
-//         const char *filename = "bob_to_alice";
-//         if (access(filename, F_OK))
-//             mkfifo(filename, 0666);
-// 	send_fifo = open(filename, O_WRONLY);
-// 	cout << "send_fifo=" << send_fifo << endl;
-//         assert(send_fifo != 0);
-//     }
-//     assert(write(send_fifo, message, message->size) == message->size);
-// }
-
-// const Message *recv()
-// {
-//     //static int fifo = 0;
-//     if (recv_fifo == 0)
-//     {
-//         const char *filename = "alice_to_bob";
-//         if (access(filename, F_OK))
-//             mkfifo(filename, 0666);
-// 	recv_fifo = open(filename, O_RDONLY);
-// 	cout << "recv_fifo=" << recv_fifo << endl;
-//         assert(recv_fifo != 0);
-//     }
-//     static Message *m = (Message *)malloc(MESSAGE_SIZES[4]);
-//     assert(read(recv_fifo, m, sizeof(Message)) == sizeof(Message));
-//     assert(read(recv_fifo, m->payload, m->payload_size()) == m->payload_size());
-//     return m;
-// }
 static Message *m = (Message *)malloc(MESSAGE_SIZES[4]);
 static int messageLen = sizeof(Message);
 void send()
 {
-    //static int send_fifo = 0;
     if (send_fifo == 0)
     {
         const char *filename = "bob_to_alice";
@@ -55,7 +22,6 @@ void send()
 
 void recv()
 {
-    //static int fifo = 0;
     if (recv_fifo == 0)
     {
         const char *filename = "alice_to_bob";
@@ -65,31 +31,22 @@ void recv()
 	cout << "recv_fifo=" << recv_fifo << endl;
         assert(recv_fifo != 0);
     }
-    //static Message *m = (Message *)malloc(MESSAGE_SIZES[4]);
     assert(read(recv_fifo, m, messageLen) == messageLen);
     assert(read(recv_fifo, m->payload, m->payload_size()) == m->payload_size());
-//     return m;
 }
 
 int main()
 {
     cout<<"bob start..."<<endl;
-    //Message *m2 = (Message *)malloc(MESSAGE_SIZES[4]);
     while (true)
     {
-//         const Message *m1 = recv();
-//         assert(m1->checksum == crc32(m1));
-//         memcpy(m2, m1, m1->size); // 拷贝m1至m2
-//         m2->payload[0]++;         // 第一个字符加一
-//         m2->checksum = crc32(m2); // 更新校验和
-// 	send(m2);
 	recv();
-	//cout<<"bob recv"<<m<<endl;
+	cout<<"bob recv"<<m<<endl;
         assert(m->checksum == crc32(m));
         m->payload[0]++;         // 第一个字符加一
         m->checksum = crc32(m); // 更新校验和
 	send();
-	//cout<<"bob send"<<m<<endl;
+	cout<<"bob send"<<m<<endl;
     }
 
     return 0;
