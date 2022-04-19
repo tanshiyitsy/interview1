1764edc1101d72c7dfba026caf6cfc0439142c96
tanshiyitsy <1365519162@qq.com>
2022-04-19 21:24:27 +0800

    Update bob.cpp

--- bob.cpp
+++ bob.cpp
@@ -9,15 +9,6 @@ static int messageLen = sizeof(Message);
 static int payload_size = 0;
 void send()
 {
-//     if (send_fifo == 0)
-//     {
-//         const char *filename = "bob_to_alice";
-//         if (access(filename, F_OK))
-//             mkfifo(filename, 0666);
-// 	send_fifo = open(filename, O_WRONLY);
-// 	cout << "send_fifo=" << send_fifo << endl;
-//         assert(send_fifo != 0);
-//     }
     assert(write(send_fifo, m, m->size) == m->size);
 }
 void sendfirst()
@@ -44,15 +35,6 @@ void recvfirst()
 }
 void recv()
 {
-//     if (recv_fifo == 0)
-//     {
-//         const char *filename = "alice_to_bob";
-//         if (access(filename, F_OK))
-//             mkfifo(filename, 0666);
-// 	recv_fifo = open(filename, O_RDONLY);
-// 	cout << "recv_fifo=" << recv_fifo << endl;
-//         assert(recv_fifo != 0);
-//     }
     assert(read(recv_fifo, m, messageLen) == messageLen);
     payload_size = m->payload_size();
     assert(read(recv_fifo, m->payload, payload_size) == payload_size);
