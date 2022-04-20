49741b924ec01d53283741be9d8290656854bc0e
tanshiyitsy <1365519162@qq.com>
2022-04-20 14:08:39 +0800

    Update alice.cpp

--- alice.cpp
+++ alice.cpp
@@ -160,7 +160,7 @@ void recv_shared_init() {
 	shm = shmat(shmid, (void*)0, 0);        //返回共享存储段连接的实际地址
 	assert(shm != (void*)-1);
 	recv_shared = (struct Shared_use_st*)shm;
-	recv_shared->read_pos = 0;
+	recv_shared->read_pos = -1;
 }
 const Message *send_msg = NULL;
 // 生产是直接生产再+1， 初始化为0
@@ -196,14 +196,15 @@ const Message *recv_msg;
 void recv() {
 	while (true) {
 		assert(sem_wait(&(recv_shared->sem)) != -1);
-		if (recv_shared->read_pos != recv_shared->write_pos) {
+		int next = (recv_shared->read_pos + 1) % BUFFER_N;
+		if (next != recv_shared->write_pos) {
 			// 消费item
-			recv_msg = recv_shared->buffer[recv_shared->read_pos];
+			recv_msg = recv_shared->buffer[next];
 			if (recv_msg) {
 				record(recv_msg);
 				std::cout << "alice recv:" << recv_msg << std::endl;
 				recv_shared->buffer[recv_shared->read_pos] = NULL;
-				recv_shared->read_pos = (recv_shared->read_pos + 1) % BUFFER_N;
+				recv_shared->read_pos = next;
 			}
 		}
 		sem_post(&(recv_shared->sem)); // 释放信号量
