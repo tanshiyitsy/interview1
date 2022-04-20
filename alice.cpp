d86c7958e8effc5d3ab18eaefc332c6b199ed4ea
tanshiyitsy <1365519162@qq.com>
2022-04-20 13:44:51 +0800

    Update alice.cpp

--- alice.cpp
+++ alice.cpp
@@ -172,8 +172,8 @@ void send() {
 			while (true) {
 				assert(sem_wait(&(send_shared->sem)) != -1); // 获取信号量
 															 // 生产item到cur
-				std::cout << "alice send:" << send_msg << std::endl;
 				if (send_shared->write_pos != send_shared->read_pos && (send_shared->buffer[send_shared->write_pos] != NULL)) {
+					std::cout << "alice send:" << send_msg << std::endl;
 					send_shared->buffer[send_shared->write_pos] = send_msg;
 					send_shared->write_pos = (send_shared->write_pos + 1) % BUFFER_N;
 					sem_post(&(send_shared->sem)); // 释放信号量
