66251c690d5c2035d3f88c7d0c47c56b33f69ef2
tanshiyitsy <1365519162@qq.com>
2022-04-20 18:48:55 +0800

    Update bob.cpp

--- bob.cpp
+++ bob.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 
 static struct Shared_use_st *recv_shared = NULL;
-static struct Shared_use_st *send_shared = NULL;
 void recv_shared_init() {
 	void *shm = NULL;
 	int shmid; // 共享内存标识符
@@ -18,14 +17,15 @@ const Message *recv_msg;
 void recv() {
 	for (int i = 0; i < BUFFER_N; i++) {
 	// 消费item
-	if (send_shared->status[i] == 1) {
+	if (recv_shared->status[i] == 1) {
 		recv_msg = (Message *)recv_shared->buffer[i];
-		//std::cout << "alice recv:" << recv_msg->payload << std::endl;
+		std::cout << "alice recv:" << recv_msg->payload << std::endl;
 		assert(recv_msg->checksum == crc32(recv_msg));
 		Message *temp = const_cast<Message *>(recv_msg);
 		temp->payload[0]++;
 		temp->checksum = crc32(temp);
 		
+		std::cout << "temp" << temp->payload << std::endl;
 		//send(); // 直接在共享内存改了
 		recv_shared->mtx.lock();
 		recv_shared->status[i] = 2;
