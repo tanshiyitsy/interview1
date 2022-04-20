f0b51e1086db8f57a8e99f031c6151a7b6a3402b
tanshiyitsy <1365519162@qq.com>
2022-04-20 19:33:18 +0800

    Update bob.cpp

--- bob.cpp
+++ bob.cpp
@@ -42,7 +42,8 @@ int main() {
 			// 消费item
 			if (recv_shared->status[i] == 1) {
 				recv_msg = (Message *)recv_shared->buffer[i];
-				//std::cout << "bob recv:" << recv_msg->payload << std::endl;
+// 				std::cout << "bob recv:" << recv_msg->payload << std::endl;
+				std::cout << "bob recv:" << i << std::endl;
 				assert(recv_msg->checksum == crc32(recv_msg));
 				Message *temp = const_cast<Message *>(recv_msg);
 				temp->payload[0]++;
